72a0e2ccf6ad530a18521979776453574f6fb3ae
Bhavikaben Sudani <sidhnakrani1911@gmail.com>
2026-02-04 11:19:43 +0100

    frontend set up and installation

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600 bg-green-500">
+        Full Stack App 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
